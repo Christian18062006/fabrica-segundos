e47ded365df93d12323a7dbc7bc10324b8d86221
Carlos.Christian <carlos.programation@hotmail.comgit config --global user.email carlos.programation@hotmail.com>
2024-03-11 22:29:40 -0300

    solicitando quantidade de segundos ao usuário e variável declarada.

--- Program.cs
+++ Program.cs
@@ -6,8 +6,10 @@ namespace SegundosFábrica
     {
         static void Main(string[] args)
         {
+            int segundos;
             Console.WriteLine("Transformador de segundos para hora, minútos e segundos.");
-
+            Console.WriteLine("digite a quantidade de segundos.");
+            segundos = int.Parse(Console.ReadLine());
         }
     }
 }
