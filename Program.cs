f38499f65cbf49bd57b04cf0526560cac266e9e3
Carlos.Christian <carlos.programation@hotmail.comgit config --global user.email carlos.programation@hotmail.com>
2024-03-11 22:28:01 -0300

    projeto iniciado com git ignore

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SegundosFábrica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Transformador de segundos para hora, minútos e segundos.");
+
+        }
+    }
+}
