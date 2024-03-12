a5ec5ad8ba256d44358e8bf4b2aef7e8f6fbcc97
Carlos.Christian <carlos.programation@hotmail.comgit config --global user.email carlos.programation@hotmail.com>
2024-03-11 22:50:58 -0300

    criada a função para transformar hora em segundos e minútos e imprimindo na tela. fim do programa

--- Program.cs
+++ Program.cs
@@ -4,12 +4,22 @@ namespace SegundosFábrica
 {
     class Program
     {
+        public static string calculaHora(int segundos)
+        {
+            double hora= segundos / 3600;
+            double minutos = (segundos%3600)/60;
+            double segundosHora = (segundos%3600) %60;
+            string horaFinal = $"Tempo total: {hora.ToString("F2")} horas : {minutos.ToString("F2")} minútos : {segundosHora.ToString("F2")} segundos.";
+            return (horaFinal);
+        }
         static void Main(string[] args)
         {
             int segundos;
             Console.WriteLine("Transformador de segundos para hora, minútos e segundos.");
             Console.WriteLine("digite a quantidade de segundos.");
             segundos = int.Parse(Console.ReadLine());
+            string converte = calculaHora(segundos);
+            Console.WriteLine(converte);
         }
     }
 }
